5246eadbc532945ebe4241ed3bf3cb86c7726328
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 11:07:04 +0100

    Exercice 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merci de renseigner votre âge.");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Vous avez " + age + " ans, vous êtes donc majeur.e.");
+            }
+        else
+        {
+                Console.WriteLine("Vous avez " + age + " ans, vous êtes donc mineur.e.");
+            }
+}
+}
+}
+
